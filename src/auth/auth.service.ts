b9022967f3347431dbaa3030a38a35e5ad46028e
Bohdan-Dubravin <dubravin111@gmail.com>
2023-02-21 21:36:08 +0200

    complete auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  HttpException,
+  HttpStatus,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserDto } from 'src/user/user.dto';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
+import { Users } from 'src/user/user.entity';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  constructor(private userService: UserService, 
-    private jwtService: JwtService){}
+  async login(userData: UserDto) {
+    const user = await this.validateUser(userData);
 
-  async login(userData: UserDto){}
-
-
-  async register(userData:UserDto){
-    const canditate = await this.userService.getUsers
+    return this.generateToken(user);
   }
 
-  
+  async register(userData: UserDto) {
+    const existUser = await this.userService.getUsersByEmail(userData.email);
+
+    if (existUser) {
+      throw new HttpException('Email already exist', HttpStatus.BAD_REQUEST);
+    }
+
+    const hashPassword = await bcrypt.hash(userData.password, 5);
+
+    const newUser = await this.userService.createUser({
+      password: hashPassword,
+      email: userData.email,
+    });
+
+    return this.generateToken(newUser);
+  }
+
+  async generateToken(user: Users) {
+    const payload = { email: user.email, id: user.id, roles: user.roles };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+
+  async validateUser(userData: UserDto) {
+    const user = await this.userService.getUsersByEmail(userData.email);
+
+    const passwordEquals = await bcrypt.compare(
+      userData.password,
+      user.password,
+    );
+
+    if (user && passwordEquals) {
+      return user;
+    }
+
+    throw new UnauthorizedException({ message: 'Incorrect email or password' });
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,28 +8,29 @@ import { Users } from './user.entity';
 
 @Injectable()
 export class UserService {
-
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
-    private rolesService: RolesService
+    private rolesService: RolesService,
   ) {}
-
 
   async createUser(user: UserDto) {
     const newUser = new Users();
-    newUser.email = user.email
-    newUser.password = user.password
+    newUser.email = user.email;
+    newUser.password = user.password;
 
-    const role = await this.rolesService.getRoleByValue('USER')
-  
+    const role = await this.rolesService.getRoleByValue('USER');
 
     newUser.roles = [role];
 
-   return await this.usersRepository.save(newUser);
+    return await this.usersRepository.save(newUser);
   }
 
   async getUsers() {
-    return await this.usersRepository.find({relations: {roles: true}});
+    return await this.usersRepository.find({ relations: { roles: true } });
+  }
+
+  async getUsersByEmail(email: string) {
+    return await this.usersRepository.findOneBy({ email });
   }
 }
