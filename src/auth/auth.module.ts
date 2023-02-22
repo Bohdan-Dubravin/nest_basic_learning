729a2635d73809b0d6e59dd22751102e99e5fddd
Bohdan-Dubravin <dubravin111@gmail.com>
2023-02-22 09:05:59 +0200

    giard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
-  imports: [  ConfigModule.forRoot({
-    isGlobal: true,
-  }),
-  JwtModule.register({secret: process.env.PRIVATE_KEY || 'secret', 
-  signOptions: 
-  {expiresIn : '24h'}}), 
-  UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY || 'secret',
+      signOptions: { expiresIn: '24h' },
+    }),
+    forwardRef(() => UserModule),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [JwtAuthGuard],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './user.entity';
 import { RolesModule } from 'src/roles/roles.module';
 import { Roles } from 'src/roles/roles.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Users, Roles]), RolesModule],
+  imports: [
+    TypeOrmModule.forFeature([Users, Roles]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
   providers: [UserService],
   controllers: [UserController],
-  exports: [UserService]
+  exports: [UserService],
 })
 export class UserModule {}
